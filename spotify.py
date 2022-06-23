0a6f6d22582e30a3caa046be87ed14b629c91a0c
cherman23 <85960019+cherman23@users.noreply.github.com>
2022-06-23 13:47:42 -0400

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -1,18 +1,50 @@
-import spotipy
-import spotipy.util as util
+import requests
+from requests.auth import HTTPBasicAuth
 
+CLIENT_ID="018eabe29a6b4e02bf97db6152da27b8"
+CLIENT_SECRET="a39175b2f95740289cf696b5408e82a4"
+TOKEN=""
 
-def main():
-    print("Welcome to spotify search!")
-    artistName = input(print("Enter artist name: "))
+print("Welcome")
 
-    artistList = spotipy.search(q=artistName, type="artist", limit=3)
-    for i in range(len(artistList)):
-        print(i+1 + ". " + artistList[i])
+def login():
+        global TOKEN
+        url = "https://accounts.spotify.com/api/token"
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        form = {
+           "grant_type":"client_credentials"
+        }
+        res = requests.post(url, headers= headers, auth=HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET), data=form)
+        if(res.status_code == 200):
+                ## save token
+                ## redirect to artists search bar
+                TOKEN = res.json()
+                return TOKEN
+        else:
+                return res.status_code
 
-    choice = input("Pick one of the artists by number: ")
-    songList = spotipy.artist_track(q=artistList[choice-1], type="track", limit=3)
-    for i in range(len(songList)):
-        print(i+1 + ". " + songList[i])
+def search():
+    login()
+    artist = input("Enter arist name: ")
+    url ="https://api.spotify.com/v1/search"
+    payload= {
+            "q": artist,
+            "type": "artist",
+            "limit": 3
+    }
+    headers = {
+            "Content-Type": "application/json", 
+            "Authorization": "Bearer " + TOKEN["access_token"]
+    }
+    res = requests.get(url, headers= headers, params=payload)
+    if(res.status_code == 200):
+            return res.json()
+    else:
+            return res.status_code
+    ## some button here to get artist name
+    ## query spotify for artists
+    ## present the top 3 to user
+    ## user selects artist
 
-main()
+search()
+
